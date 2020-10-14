cf6239e6b9d8cbd4e42e22fb778828a8ebdc53d7
Jens-Kristian Krogager <jens-kristian@krogager.dk>
2020-10-14 07:59:30 +0200

    Updated redQSO webpage
    Updated broken links and references

--- redQSOs/upload.sh
+++
@@ -1,3 +0,0 @@
-scp data.html redQSOs_data.js krogager@dark03.dark-cosmology.dk:~/Sites/redQSOs/.
-scp images/*.png krogager@dark03.dark-cosmology.dk:~/Sites/redQSOs/images/.
-scp SEDs/*.png krogager@dark03.dark-cosmology.dk:~/Sites/redQSOs/SEDs/.
